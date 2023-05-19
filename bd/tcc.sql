16ee398dff3cc7f91f35f2de22631180f56ec70b
Dodoh04 <douglasoliversa3@gmail.com>
2023-05-19 20:58:33 -0300

    script do tcc funcional

--- bd/tcc.sql
+++ bd/tcc.sql
@@ -1,131 +1,37 @@
-CREATE DATABASE tcc;
+
+create database Tcc;
 
 
-create table tcc.Produtos(
-    IdProduto int not null primary key auto_increment,
-    NomeProd varchar(50) not null,
-    QtndProd int not null,
-    DataAtualProd date not null
+CREATE TABLE IF NOT EXISTS `Tcc`.`produtos` (
+  `IdProduto` int NOT NULL AUTO_INCREMENT,
+  `NomeProd` varchar(50) NOT NULL,
+  `QtndProd` int NOT NULL,
+  `DataAtualProd` date NOT NULL,
+  PRIMARY KEY (`IdProduto`)
 );
 
-create table tcc.Funcionario(
-    IdFuncionario int not null primary key auto_increment,
-    NomeFunc varchar(50) not null,
-    CPFFunc varchar(14) not null,
-    EmailFunc varchar(50) not null,
-    SenhaFuncionario varchar(20) not null
+
+INSERT INTO `tcc`.`produtos` (`IdProduto`, `NomeProd`, `QtndProd`, `DataAtualProd`) VALUES
+(3, 'Pomadinha', 32, '2023-05-19'),
+(4, 'Ibuprofeno', 100, '2023-05-12'),
+(5, 'Losartana', 90, '2023-05-03');
+
+
+CREATE TABLE IF NOT EXISTS `Tcc`.`usuario` (
+  `cpf` varchar(11) NOT NULL,
+  `nome` varchar(20) NOT NULL,
+  `sobrenome` varchar(20) NOT NULL,
+  `nivelAcess` varchar(15) NOT NULL,
+  `sexo` varchar(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `senha` varchar(10) NOT NULL,
+  PRIMARY KEY (`cpf`),
+  UNIQUE KEY `senha` (`senha`)
 );
 
-create table tcc.Clientes(
-    IdCliente int not null primary key auto_increment,
-    NomeCli varchar(50) not null,
-    CPFCli varchar(14) not null,
-    EmailCli varchar(50) not null
-);
-CREATE TABLE `tcc`.`usuario` (
-    `idUsuario` INT NOT NULL AUTO_INCREMENT ,
-    `nome` VARCHAR(30) NOT NULL ,
-    `login` VARCHAR(35) NOT NULL ,
-    `senha` VARCHAR(10) NOT NULL ,
-    `acessUsuario` INT(1) NOT NULL ,
-       PRIMARY KEY (`idUsuario`),
-       UNIQUE (`login`),
-       UNIQUE (`senha`)
-)ENGINE = InnoDB;
-
-INSERT INTO Produtos 
-VALUES(NULL, "Doril",100, "11/09/2022"),
-(NULL, "Dipirona", 89, "11/09/2022"), 
-(NULL, "Xarelto", 4, "06/09/2022"), 
-(NULL, "Losartana", 50, "10/10/2022");
-
-INSERT INTO Clientes 
-VALUES(NULL, "Marcos da Silva",'100.200.300-33', "marcoo33@silva.com"),
-(NULL, "Jonas Nascimento", '111.222.333-55', "jonas@nacimento.br"), 
-(NULL, "Luan de Souza", '333.444.555-66', "luan@souza.gmail"), 
-(NULL, "Laurentina de Carvalho", '666.222.333-55', "laura@tina.hot"),
-(NULL, "Jose Mario de Oliveira Alves",'100.200.300-33', "marcoo33@silva.com");
-
-INSERT INTO `usuario` (`idUsuario`, `nome`, `login`, `senha`, `acessUsuario`) 
-VALUES (NULL, 'Douglas', 'douglasoliversa3@gmail.com', '123456', '1'), 
-(NULL, 'teste', 'teste.test@gmail.com', '123', '0');
+INSERT INTO `tcc`.`usuario` (`cpf`, `nome`, `sobrenome`, `nivelAcess`, `sexo`, `email`, `senha`) VALUES
+('11111111111', 'Douglas', 'Oliveira', 'Administrador', 'Masculino', 'douglasAdm@test.com', 'adm'),
+('22222222222', 'Cliente', 'Teste', 'Cliente', 'Feminino', 'testCliente@.com', 'cliente');
+COMMIT;
 
 
-
-create table Produtos(
-    IdProduto int not null primary key auto_increment,
-    NomeProd varchar(50) not null,
-    QtndProd int not null,
-    DataAtualProd date not null
-);
-
-create table Funcionario(
-    IdFuncionario int not null primary key auto_increment,
-    NomeFunc varchar(50) not null,
-    CPFFunc varchar(14) not null,
-    EmailFunc varchar(50) not null,
-    SenhaFuncionario varchar(20) not null
-);
-
-create table Clientes(
-    IdCliente int not null primary key auto_increment,
-    NomeCli varchar(50) not null,
-    CPFCli varchar(14) not null,
-    EmailCli varchar(50) not null
-);
-CREATE TABLE usuario(
-    `idUsuario` INT NOT NULL AUTO_INCREMENT ,
-    `nome` VARCHAR(30) NOT NULL ,
-    `login` VARCHAR(35) NOT NULL ,
-    `senha` VARCHAR(10) NOT NULL ,
-    `acessUsuario` INT(1) NOT NULL ,
-       PRIMARY KEY (`idUsuario`),
-       UNIQUE (`login`),
-       UNIQUE (`senha`)
-)ENGINE = InnoDB;
-
-INSERT INTO Produtos 
-VALUES(NULL, "Doril",100, "11/09/2022"),
-(NULL, "Dipirona", 89, "11/09/2022"), 
-(NULL, "Xarelto", 4, "06/09/2022"), 
-(NULL, "Losartana", 50, "10/10/2022");
-
-INSERT INTO Clientes 
-VALUES(NULL, "Marcos da Silva",'100.200.300-33', "marcoo33@silva.com"),
-(NULL, "Jonas Nascimento", '111.222.333-55', "jonas@nacimento.br"), 
-(NULL, "Luan de Souza", '333.444.555-66', "luan@souza.gmail"), 
-(NULL, "Laurentina de Carvalho", '666.222.333-55', "laura@tina.hot"),
-(NULL, "Jose Mario de Oliveira Alves",'100.200.300-33', "marcoo33@silva.com");
-
-INSERT INTO `usuario` (`idUsuario`, `nome`, `login`, `senha`, `acessUsuario`) 
-VALUES (NULL, 'Douglas', 'douglasoliversa3@gmail.com', '123456', '1'), 
-(NULL, 'teste', 'teste.test@gmail.com', '123', '0');
-
-
-nome 
-sobrenomne
-adm
-Funcionario
-farmaceutico
-CPF
-email 
-sexo
-usuario
-senha
-
-CREATE TABLE usuario(
-    `cpf` varchar(11) NOT NULL,
-    `nome` VARCHAR(20) NOT NULL ,
-    `sobrenome` VARCHAR(20) NOT NULL ,
-    `adm` INT NOT NULL,
-    `func` INT NOT NULL,
-    `famrceutico` INT NOT NULL,
-    `sexo` VARCHAR(10) NOT NULL,
-    `email` VARCHAR(50) NOT NULL,
-    `login` VARCHAR(35) NOT NULL,
-    `senha` VARCHAR(10) NOT NULL,
-    `acessUsuario` INT(1) NOT NULL,
-       PRIMARY KEY (`cpf`),
-       UNIQUE (`login`),
-       UNIQUE (`senha`)
-)ENGINE = InnoDB;
