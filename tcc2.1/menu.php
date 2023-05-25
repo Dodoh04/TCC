a9b9ad6284b0429b19be5f6032ae1fafa5ed9fc5
Dodoh04 <douglasoliversa3@gmail.com>
2023-05-24 22:08:20 -0300

    iniciando novo design

---
+++ tcc2.1/menu.php
@@ -0,0 +1,8 @@
+<nav>
+    <div><img src="img/logotcc.png" alt=""></div>
+    <ul>
+        <li><a href="">MEDICAMENTOS</a></li>
+        <li><a href="">CLIENTES</a></li>
+        <li><a href="">COLABORADORES</a></li>
+    </ul>
+</nav>
