279ef84492194e18576a9e5adb4dec64bc0c9b17
Douglas Oliveira <douglasoliversa3@gmail.com>
2023-05-26 16:58:56 -0300

    inicio da estilização do sistema

---
+++ tcc2.1/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <style>
+        *{
+            margin: 0px;
+        }
+    </style>
+
+</head>
+<body>
+    
+    
+        <div class="menu">
+            <div class="logo"><img src="img/logotcc.png" alt=""></div>
+                <nav class="nav">
+                    <ul>
+                        <li><a href="">MEDICAMENTOS</a></li>
+                        <li><a href="">CLIENTES</a></li>
+                        <li><a href="">COLABORADORES</a></li>
+                    </ul>
+                </nav>
+            <div class="search">
+                <input type="search">
+                    
+                        <a href="#">
+                            <img src="img/lupa.png" alt="">
+                        </a>
+                    
+            </div>
+            <div class="imgUser">
+                <a href=""><img src="img/user.png" alt=""></a>
+            </div>
+        </div>
+    
+    <section>
+        <div class="container">
+            <div class="card">
+                <div class="conteudo">
+                    <img src="img/iconMedi.png" alt="">
+                    <a href="#">NOME DO REMEDIO</a>
+                    <p>300</p>
+                </div>
+            </div>
+        </div>
+
+        <a href="" class="botaoAdd">ADICIONAR</a>
+    </section>
+
+    <footer class="rodape">
+
+        <div class="logoRodape"><img src="img/logoTcc.png" alt=""></div>
+
+        <div class="links">
+
+            <ul>
+                <h4>INICIO</h4>
+                <li><a href="">MEDICAMENTOS</a></li>
+                <li><a href="">CLIENTES</a></li>
+                <li><a href="">COLABORADORES</a></li>
+            </ul>
+        </div>
+
+        <div class="suporte">
+            <h4>SUPORTE</h4>
+            <p>contato@contato.com <img src="img/iconMsgm.png" alt=""></p>
+            <p>(11) 94555-5555 <img src="img/iconWpp.png" alt=""></p>
+        </div>
+    </footer>
+</body>
+</html>
--- tcc2.1/menu.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <div><img src="img/logotcc.png" alt=""></div>
-    <ul>
-        <li><a href="">MEDICAMENTOS</a></li>
-        <li><a href="">CLIENTES</a></li>
-        <li><a href="">COLABORADORES</a></li>
-    </ul>
-</nav>
